e2a078f32c0814bce6f3076528325edf6e8a7bb9
Vera Simon <vera.simon01@gmail.com>
2019-03-29 11:00:44 -0700

    lsls Having some issues getting the file size properly, but previous commit was working for names as expected

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
 
@@ -7,6 +8,7 @@
  */
 int main(int argc, char **argv)
 {
+    char *path;
     // Set up that directory pointer
     DIR *dir;
     struct dirent *ent;
@@ -15,27 +17,37 @@ int main(int argc, char **argv)
     // argv argument 0 is the app name itself
     if (argc > 1)
     {
-        // Open directory
-        dir = opendir(argv[1]);
-        if (dir == NULL)
-        {
-            printf("Invalid directory: '%s'\n", argv[1]);
-            return 1;
-        }
+        path = argv[1];
     }
     else
     {
-        // Open directory
-        dir = opendir(".");
+        path = ".";
+    }
+
+    dir = opendir(path);
+    if (dir == NULL)
+    {
+        printf("Invalid directory: '%s'\n", argv[1]);
+        exit(1);
     }
 
     // Repeatly read and print entries
     while ((ent = readdir(dir)) != NULL)
     {
-        printf("%s\n", ent->d_name);
+        char p2f[sizeof(long long int)];
+        struct stat buf;
+
+        snprintf(p2f, sizeof(p2f), "%s\t%s/%s", buf.st_size, path, ent->d_name);
+        if (stat(path, &buf) < 0)
+        {
+            fprintf(2, "File stat failed for %s\n", path);
+            exit(1);
+        };
+        // printf("%lld\t%s\n", stat(*ent, &buf), ent->d_name);
     }
 
     // Close directory
     closedir(dir);
+
     return 0;
 }
