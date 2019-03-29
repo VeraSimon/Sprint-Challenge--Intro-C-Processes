cd2a9acd000f8bb6ba0088ff74ff770989d6a55b
Vera Simon <vera.simon01@gmail.com>
2019-03-29 10:03:32 -0700

    lsls Printing file names as expected from current, or when specified, another directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+    // Set up that directory pointer
+    DIR *dir;
+    struct dirent *ent;
 
-  // Open directory
+    // Parse command line
+    // argv argument 0 is the app name itself
+    if (argc > 1)
+    {
+        // Open directory
+        dir = opendir(argv[1]);
+        if (dir == NULL)
+        {
+            printf("Invalid directory: '%s'\n", argv[1]);
+            return 1;
+        }
+    }
+    else
+    {
+        // Open directory
+        dir = opendir(".");
+    }
 
-  // Repeatly read and print entries
+    // Repeatly read and print entries
+    while ((ent = readdir(dir)) != NULL)
+    {
+        printf("%s\n", ent->d_name);
+    }
 
-  // Close directory
-
-  return 0;
+    // Close directory
+    closedir(dir);
+    return 0;
 }
